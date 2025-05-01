397afa5ff18ede391f414cec6d442ac06db09f0e
AmirAbu-Almajd <amir.abualmajd99@gmail.com>
2025-05-01 19:38:28 +0200

    Adding API project

---
+++ Inventory Management Backend/InventoryManagement.API/Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+﻿using InventoryManagement.Application.DTOs;
+using InventoryManagement.Application.Interfaces;
+using InventoryManagement.Application.Services;
+using InventoryManagement.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagement.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productsService;
+
+        public ProductController(IProductService productsService)
+        {
+            _productsService = productsService;
+        }
+
+        [HttpGet(Name = $"{nameof(ProductService.GetAllProductsAsync)}")]
+        public async Task<ActionResult<IEnumerable<Product>>> GetAll()
+        {
+            var products = await _productsService.GetAllProductsAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetById(Guid id)
+        {
+            var product = await _productsService.GetProductByIdAsync(id);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(ProductDto dto)
+        {
+            await _productsService.AddProductAsync(dto);
+            return CreatedAtAction(nameof(GetAll), null);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(Guid id, ProductDto dto)
+        {
+            await _productsService.UpdateProductAsync(id, dto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            await _productsService.DeleteProductAsync(id);
+            return NoContent();
+        }
+    }
+}
