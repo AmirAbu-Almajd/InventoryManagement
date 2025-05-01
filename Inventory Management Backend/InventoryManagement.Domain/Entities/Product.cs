45712b9c851ed8084d6ff8bd7878034fe221cb29
AmirAbu-Almajd <amir.abualmajd99@gmail.com>
2025-05-01 23:14:35 +0200

    Adjusting entity to produce first result

--- Inventory Management Backend/InventoryManagement.Domain/Entities/Product.cs
+++ Inventory Management Backend/InventoryManagement.Domain/Entities/Product.cs
@@ -1,16 +1,16 @@
 ﻿using InventoryManagement.Domain.Commons;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace InventoryManagement.Domain.Entities
 {
     public class Product : FullAuditedEntity
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
         public Guid Id { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
---
+++ Inventory Management Backend/InventoryManagement.Infrastructure/Migrations/20250501204833_Update_Product_Table.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProductTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Products",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NEWSEQUENTIALID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Products");
+        }
+    }
+}
