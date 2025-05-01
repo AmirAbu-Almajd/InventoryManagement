db2be76e18d000759a2dcc447d20d4bd10f66d71
AmirAbu-Almajd <amir.abualmajd99@gmail.com>
2025-05-01 21:49:00 +0200

    Adding Frontend  project

---
+++ Inventory-Management-Frontend/src/app/services/product.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Product {
+  id: string;
+  code: string;
+  name: string;
+}
+
+export interface ProductDto {
+  code: string;
+  name: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private apiUrl = 'https://localhost:44373/api/product'; 
+
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
+  }
+
+  getProduct(id: string): Observable<Product> {
+    return this.http.get<Product>(`${this.apiUrl}/${id}`);
+  }
+
+  createProduct(product: ProductDto): Observable<any> {
+    return this.http.post(this.apiUrl, product);
+  }
+
+  updateProduct(id: string, product: ProductDto): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, product);
+  }
+
+  deleteProduct(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
